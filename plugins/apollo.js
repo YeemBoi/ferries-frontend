f50a614664547c7b7eced36f885c380784ba15f6
Thomas E <einstein.thomas2@gmail.com>
2021-10-18 19:36:22 -0700

    using heroku

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -1,6 +1,6 @@
 export default (context) => {
     return {
-        httpEndpoint:        process.env.INTERNAL_GRAPHQL_URL || 'http://ferries-backend.herokuapp.com/graphql',
-        browserHttpEndpoint: process.env.EXTERNAL_GRAPHQL_URL || 'http://ferries-backend.herokuapp.com/graphql',
+        httpEndpoint:        process.env.INTERNAL_GRAPHQL_URL || 'https://ferries-backend.herokuapp.com/graphql',
+        browserHttpEndpoint: process.env.EXTERNAL_GRAPHQL_URL || 'https://ferries-backend.herokuapp.com/graphql',
     }
 }
